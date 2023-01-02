cc2e1ec5749922f95a099e42ef2754ada3f1d4b8
hillal20 <>
2023-01-02 20:55:28 +0000

    first commit

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("hello word")
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req: any, res:any )=> {
+  res.send("api is working ")
+})
+app.listen(5000, ()=> {
+  console.log("-- api is running on 5000 -- ")
+})
