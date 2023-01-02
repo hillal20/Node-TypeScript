1d42fbfdfbe29df632d91923a97bbb9f3464e2b1
hilal aissani <hilalaissani@gmail.com>
2023-01-02 22:25:37 +0000

    api is working

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 const express = require('express');
+const { Request , Response, NextFunction } = express;
 const app = express();
+app.use(express.json());
 
 
+app.use((req:Request , res:Response  , next: NextFunction ) => {
+  next();
+})
 
+app.get("/", (req: Request, res:Response)=> {
+  res.status(200).json({data: "api is working "});
+})
 
-app.get("/", (req: any, res:any )=> {
+app.post("/hello", (req: any, res:any )=> {
   res.send("api is working ")
 })
+
 app.listen(5000, ()=> {
   console.log("-- api is running on 5000 -- ")
-})
+});
